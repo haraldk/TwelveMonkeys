f610c82cb754e9bea7a1547943cd2ce96c6f2451
Harald Kuhr <harald.kuhr@gmail.com>
2010-10-13 17:04:11 +0200

    Updated some copyright statements.

--- sandbox/sandbox-common/src/main/java/com/twelvemonkeys/image/MappedFileBuffer.java
+++ sandbox/sandbox-common/src/main/java/com/twelvemonkeys/image/MappedFileBuffer.java
@@ -1,4 +1,34 @@
+/*
+ * Copyright (c) 2010, Harald Kuhr
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *    * Neither the name "TwelveMonkeys" nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.twelvemonkeys.image;
+
+import com.twelvemonkeys.lang.Validate;
 
 import java.awt.image.DataBuffer;
 import java.io.File;
@@ -22,11 +52,7 @@ public abstract class MappedFileBuffer extends DataBuffer {
     private final Buffer buffer;
 
     private MappedFileBuffer(final int type, final int size, final int numBanks) throws IOException {
-        super(type, size, numBanks);
-
-        if (size < 0) {
-            throw new IllegalArgumentException("Integer overflow for size: " + size);
-        }
+        super(type, Validate.isTrue(size < 0, size, "Integer overflow for size: %d"), numBanks);
 
         int componentSize = DataBuffer.getDataTypeSize(type) / 8;
 
