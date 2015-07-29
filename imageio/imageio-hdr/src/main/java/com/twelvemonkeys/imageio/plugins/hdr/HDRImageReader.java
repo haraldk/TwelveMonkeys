bef0b762fafe002002ad34ca1840ab54c1c37a84
Harald Kuhr <harald.kuhr@gmail.com>
2015-07-29 09:57:33 +0200

    TMI-157: Added license headers, and readRaster method for access to the RGBE data.

--- imageio/imageio-hdr/src/main/java/com/twelvemonkeys/imageio/plugins/hdr/HDRImageReader.java
+++ imageio/imageio-hdr/src/main/java/com/twelvemonkeys/imageio/plugins/hdr/HDRImageReader.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2015, Harald Kuhr
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name "TwelveMonkeys" nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.twelvemonkeys.imageio.plugins.hdr;
 
 import com.twelvemonkeys.imageio.ImageReaderBase;
@@ -13,6 +41,7 @@ import java.awt.*;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
@@ -141,6 +170,69 @@ public final class HDRImageReader extends ImageReaderBase {
         processImageComplete();
 
         return destination;
+    }
+
+    @Override
+    public boolean canReadRaster() {
+        return true;
+    }
+
+    @Override
+    public Raster readRaster(final int imageIndex, final ImageReadParam param) throws IOException {
+        checkBounds(imageIndex);
+        readHeader();
+
+        int width = getWidth(imageIndex);
+        int height = getHeight(imageIndex);
+
+        Rectangle srcRegion = new Rectangle();
+        Rectangle destRegion = new Rectangle();
+        computeRegions(param, width, height, null, srcRegion, destRegion);
+        destRegion = srcRegion; // We don't really care about destination for raster
+
+        BufferedImage destination = new BufferedImage(srcRegion.width, srcRegion.height, BufferedImage.TYPE_4BYTE_ABGR);
+        WritableRaster raster = destination.getRaster();
+
+        int xSub = param != null ? param.getSourceXSubsampling() : 1;
+        int ySub = param != null ? param.getSourceYSubsampling() : 1;
+
+        byte[] rowRGBE = new byte[width * 4];
+        byte[] pixelRGBE = new byte[width];
+
+        processImageStarted(imageIndex);
+
+        // Process one scanline of RGBE data at a time
+        for (int srcY = 0; srcY < height; srcY++) {
+            int dstY = ((srcY - srcRegion.y) / ySub) + destRegion.y;
+            if (dstY >= destRegion.height) {
+                break;
+            }
+
+            RGBE.readPixelsRawRLE(imageInput, rowRGBE, 0, width, 1);
+
+            if (srcY % ySub == 0 && dstY >= destRegion.y) {
+                for (int srcX = srcRegion.x; srcX < srcRegion.x + srcRegion.width; srcX += xSub) {
+                    int dstX = ((srcX - srcRegion.x) / xSub) + destRegion.x;
+                    if (dstX >= destRegion.width) {
+                        break;
+                    }
+
+                    System.arraycopy(rowRGBE, srcX * 4, pixelRGBE, 0, 4);
+                    raster.setDataElements(dstX, dstY, pixelRGBE);
+                }
+            }
+
+            processImageProgress(srcY * 100f / height);
+
+            if (abortRequested()) {
+                processReadAborted();
+                break;
+            }
+        }
+
+        processImageComplete();
+
+        return destination.getRaster();
     }
 
     @Override
