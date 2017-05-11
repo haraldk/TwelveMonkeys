38700326d60377708e78eede5f1cacf86e60072e
Kumar Raman <kumar.raman@yahoo.com>
2017-05-11 11:56:29 -0700

    Changed codesigning.asc.enc

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_13d7419eac8d_key -iv $encrypted_13d7419eac8d_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_3a5819198e36_key -iv $encrypted_3a5819198e36_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
