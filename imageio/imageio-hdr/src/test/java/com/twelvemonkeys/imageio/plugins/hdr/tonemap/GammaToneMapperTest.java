5040e9fe8a8d07676fb9ce280a630229087705fb
Harald Kuhr <harald.kuhr@gmail.com>
2020-09-24 15:07:40 +0200

    Add missing tests.

---
+++ imageio/imageio-hdr/src/test/java/com/twelvemonkeys/imageio/plugins/hdr/tonemap/GammaToneMapperTest.java
@@ -0,0 +1,38 @@
+package com.twelvemonkeys.imageio.plugins.hdr.tonemap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class GammaToneMapperTest {
+    private final GammaToneMapper mapper = new GammaToneMapper();
+
+    @Test
+    public void testMap0() {
+        float[] rgb = {0};
+        mapper.map(rgb);
+        assertArrayEquals(new float[]{0}, rgb, 0);
+    }
+
+    @Test
+    public void testMap1() {
+        float[] rgb = {1};
+        mapper.map(rgb);
+        assertArrayEquals(new float[]{0.5f}, rgb, 0);
+    }
+
+    @Test
+    public void testMap16() {
+        float[] rgb = {15.999999f};
+        mapper.map(rgb);
+        assertArrayEquals(new float[]{1}, rgb, 0);
+    }
+
+    @Test
+    public void testMapMax() {
+        float[] rgb = {Float.MAX_VALUE};
+        mapper.map(rgb);
+        assertArrayEquals(new float[]{1}, rgb, 0);
+    }
+
+}
