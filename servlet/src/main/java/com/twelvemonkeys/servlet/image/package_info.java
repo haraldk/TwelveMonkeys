b0ad6b2a4b718eafe86d74977fc910d653e8a177
Harald Kuhr <harald.kuhr@gmail.com>
2021-12-14 19:35:13 +0100

    Delete deprecated Servlet classes

--- servlet/src/main/java/com/twelvemonkeys/servlet/image/package_info.java
+++ servlet/src/main/java/com/twelvemonkeys/servlet/image/package_info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Harald Kuhr
+ * Copyright (c) 2021, Harald Kuhr
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,35 +29,7 @@
  */
 
 /**
- * Contains various image-outputting filters, that should run under any
- * servlet engine.
- * <P>
- * Some of these methods may require use of the native graphics libraries
- * supported by the JVM, like the X libraries on Unix systems, and should be
- * run with JRE <STRONG>1.4</STRONG> or later, and with the option:
- * <DL>
- * <DD>{@code -Djawa.awt.headless=true}</DD>
- * </DL>
- * See the document
- * <A href="http://java.sun.com/j2se/1.4/docs/guide/awt/AWTChanges.html#headless">AWT Enhancements</A> and bugtraq report
- * <A href="http://developer.java.sun.com/developer/bugParade/bugs/4281163.html">4281163</A> for more information on this issue.
- * <P>
- * If you cannot use JRE 1.4 or later, or do not want to use the X
- * libraries, one possibility is to use the
- * <A href="http://www.eteks.com/pja/en/">PJA package</A> (com.eteks.pja),
- * and start the JVM with the following options:
- * <DL>
- * <DD>{@code -Xbootclasspath/a:&lt;path to pja.jar&gt;}</DD>
- * <DD>{@code -Dawt.toolkit=com.eteks.awt.PJAToolkit}</DD>
- * <DD>{@code -Djava.awt.graphicsenv=com.eteks.java2d.PJAGraphicsEnvironment}</DD>
- * <DD>{@code -Djava.awt.fonts=&lt;path where True Type fonts files will be loaded from&gt;}</DD>
- * </DL>
- * <P>
- * Please note that creation of PNG images (from bytes or URL's) are only
- * supported in JRE 1.3 and later, trying to load them from an earlier version,
- * will result in errors.
- *
- * @see com.twelvemonkeys.servlet.image.ImageServlet
- * @see com.twelvemonkeys.servlet.image.ImagePainterServlet
+ * Contains the context listener required for bundling ImageIO plugins
+ * inside a web application.
  */
 package com.twelvemonkeys.servlet.image;
