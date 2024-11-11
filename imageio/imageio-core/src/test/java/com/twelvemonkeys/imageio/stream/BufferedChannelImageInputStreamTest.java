62d79e5a85fa41287eb855d09066098ff71c733f
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2024-11-11 21:15:34 +0530

    Update imageio/imageio-core/src/test/java/com/twelvemonkeys/imageio/stream/BufferedChannelImageInputStreamTest.java
    Co-authored-by: Harald Kuhr <harald.kuhr@gmail.com>

--- imageio/imageio-core/src/test/java/com/twelvemonkeys/imageio/stream/BufferedChannelImageInputStreamTest.java
+++ imageio/imageio-core/src/test/java/com/twelvemonkeys/imageio/stream/BufferedChannelImageInputStreamTest.java
@@ -167,7 +167,7 @@ public class BufferedChannelImageInputStreamTest {
                 stream.seek(newPos);
                 assertEquals(newPos, stream.getStreamPosition(), "Wrong stream position");
                 stream.readFully(result);
-                assertTrue( rangeEquals(data, (int) newPos, result, 0, result.length), "Wrong data read: " + i);
+                assertTrue(rangeEquals(data, (int) newPos, result, 0, result.length), "Wrong data read: " + i);
             }
         }
     }
