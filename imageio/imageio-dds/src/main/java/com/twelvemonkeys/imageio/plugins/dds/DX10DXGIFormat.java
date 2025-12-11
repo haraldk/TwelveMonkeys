16694dd236f6608d93b56d9969bb42bd761ee8fe
KhanhTypo <68932626+KhanhPham05@users.noreply.github.com>
2025-12-11 10:22:55 +0700

    expand the supporting range for some DX10 DXGI Format in the DXGI_FORMAT enumeration

--- imageio/imageio-dds/src/main/java/com/twelvemonkeys/imageio/plugins/dds/DX10DXGIFormat.java
+++ imageio/imageio-dds/src/main/java/com/twelvemonkeys/imageio/plugins/dds/DX10DXGIFormat.java
@@ -1,25 +1,31 @@
 package com.twelvemonkeys.imageio.plugins.dds;
 
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+
 /**
  * Enum that lists a certain types of DXGI Format this reader supports to read.
- * All int values follow this DXGI_FORMAT_*_UNORM_SRGB format (with '*' being one of the enum element) are supported.
  *
  * @link <a href="https://learn.microsoft.com/en-us/windows/win32/api/dxgiformat/ne-dxgiformat-dxgi_format">DXGI Format List</a>
  */
 public enum DX10DXGIFormat {
-    BC1(72, DDSType.DXT1),
-    BC2(75, DDSType.DXT2),
-    BC3(78, DDSType.DXT5),
+    BC1(DDSType.DXT1, 70, 72),
+    BC2(DDSType.DXT2, 73, 75),
+    BC3(DDSType.DXT5, 76, 78),
     //BC7(99),
-    R8G8B8A8(29, DDSType.A8R8G8B8),
-    B8G8R8A8(91, DDSType.A8B8G8R8),
-    B8G8R8X8(93, DDSType.X8B8G8R8);
-
-    private final int dxgiFormatNum;
+    R8G8B8A8(DDSType.A8R8G8B8, 27, 32),
+    B8G8R8A8(DDSType.A8B8G8R8, 87, 90, 91),
+    B8G8R8X8(DDSType.X8B8G8R8, 88, 92, 93);
     private final DDSType ddsType;
+    private final IntPredicate dxgiFormat;
 
-    DX10DXGIFormat(int dxgiFormat, DDSType ddsType) {
-        this.dxgiFormatNum = dxgiFormat;
+    DX10DXGIFormat(DDSType ddsType, int... supportedFormats) {
+        this.ddsType = ddsType;
+        this.dxgiFormat = value -> Arrays.stream(supportedFormats).anyMatch(supportedFormat -> supportedFormat == value);
+    }
+
+    DX10DXGIFormat(DDSType ddsType, int dxgiFormatMin, int dxgiFormatMax) {
+        this.dxgiFormat = value -> dxgiFormatMin <= value && value <= dxgiFormatMax;
         this.ddsType = ddsType;
     }
 
@@ -29,9 +35,10 @@ public enum DX10DXGIFormat {
 
     static DX10DXGIFormat getFormat(int value) {
         for (DX10DXGIFormat format : values()) {
-            if (format.dxgiFormatNum == value) return format;
+            if (format.dxgiFormat.test(value)) return format;
         }
 
         throw new UnsupportedOperationException("Unsupported DXGI_FORMAT : " + value);
     }
+
 }
